4f992edeb66aedf5397ed46ac7eeda8e1452e2f7
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2024-02-23 15:57:47 -0500

    sart

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,11 +10,8 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
-import TripList from '../TripList/TripList';
+
 import './App.css';
-import MyChart from '../MyChart/echarts';
-import FamilySize from '../FamilySize/familySize';
-import Patients from '../Patients/Patients';
 
 
 
@@ -56,19 +53,8 @@ function App() {
           </Route>
           <ProtectedRoute exact path="/user" component={UserPage} />
           <ProtectedRoute exact path="/info" component={InfoPage} />
-          <ProtectedRoute exact path="/my_trips" component={TripList} />
-          <ProtectedRoute exact path="/echarts" component={MyChart} />
-
-          <Route exact path="/family_size">
-            <FamilySize />
-          </Route>
-
-          <Route exact path="/patients">
-            <Patients/>
-          </Route> 
-
-
-
+         
+  
           <Route>
 
 
@@ -86,100 +72,3 @@ function App() {
 }
 
 export default App;
-
-// import React, { useEffect } from 'react';
-// import {
-//   HashRouter as Router,
-//   Redirect,
-//   Route,
-//   Switch,
-// } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import Nav from '../Nav/Nav';
-// import Footer from '../Footer/Footer';
-// import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
-// import AboutPage from '../AboutPage/AboutPage';
-// import UserPage from '../UserPage/UserPage';
-// import InfoPage from '../InfoPage/InfoPage';
-// import LandingPage from '../LandingPage/LandingPage';
-// import LoginPage from '../LoginPage/LoginPage';
-// import RegisterPage from '../RegisterPage/RegisterPage';
-// import TripList from '../TripList/TripList';
-// import './App.css';
-// import MyChart from '../MyChart/echarts';
-// import FamilySize from '../FamilySize/familySize.jsx';
-
-
-
-
-// function App() {
-//   const dispatch = useDispatch();
-
-//   const user = useSelector(store => store.user);
-
-//   useEffect(() => {
-//     dispatch({ type: 'FETCH_USER' });
-//   }, [dispatch]);
-
-//   return (
-//     <Router>
-//       <div>
-//         <Nav />
-//         <Switch>
-//           <Redirect exact from="/" to="/home" />
-
-//           <Route exact path="/about" component={AboutPage} />
-
-//           <Route exact path="/login">
-//             {user.id ? (
-//               <Redirect to="/user" />
-//             ) : (
-//               <LoginPage />
-//             )}
-//           </Route>
-
-//           <Route exact path="/registration">
-//             {user.id ? (
-//               <Redirect to="/user" />
-//             ) : (
-//               <RegisterPage />
-//             )}
-//           </Route>
-
-//           <Route exact path="/home">
-//             {user.id ? (
-//               <Redirect to="/user" />
-//             ) : (
-//               <LandingPage />
-//             )}
-//           </Route>
-
-//           <ProtectedRoute exact path="/user" component={UserPage} />
-
-//           <ProtectedRoute exact path="/info" component={InfoPage} />
-
-//           <ProtectedRoute exact path="/my_trips" component={TripList} />
-
-//           <Route exact path="/echarts">
-//             <MyChart />
-
-//           </Route>
-//           <Route exact path="/family_size">
-//             <FamilySize />
-//           </Route>
-
-
-
-
-//           <Route>
-//             <h1>404 - Page Not Found</h1>
-//           </Route>
-//         </Switch>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,125 +3,30 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+import Avatar from '@mui/material/Avatar';
+
+
+
 
 function Nav() {
   const user = useSelector((store) => store.user);
 
   return (
     <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">
-          Prime Solo Project
-        </h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {user.id === null && (
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/echarts">
-              ECharts
-            </Link>
-            <Link className="navLink" to="/family_size">
-              Family Size
-            </Link>
-            <Link className="navLink" to="/patients">
-              Patients
-            </Link>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-            <Link className="navLink" to="/my_trips">
-              My Trips
-            </Link>
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
+      <div className="nav-title">
+      <Avatar alt="Giannina" src="/images/Giannina.png" />
+        <Link to="/home">
+          <h2>Coach Cochi</h2>
         </Link>
+      </div>
+      <div className="nav-links">
+        <Link to="/home">Home</Link>
+        <Link to="/info">Info Page</Link>
+        <Link to="/about">About</Link>
       </div>
     </div>
   );
 }
 
 export default Nav;
-
-
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import LogOutButton from '../LogOutButton/LogOutButton';
-// import './Nav.css';
-// import { useSelector } from 'react-redux';
-
-
-// function Nav() {
-//   const user = useSelector((store) => store.user);
-
-
-//   return (
-//     <div className="nav">
-//       <Link to="/home">
-//         <h2 className="nav-title">
-//           Prime Solo Project
-//         </h2>
-//       </Link>
-//       <div>
-//         {/* If no user is logged in, show these links */}
-//         {user.id === null && (
-//           <Link className="navLink" to="/login">
-//             Login / Register
-//           </Link>
-//         )}
-
-//         {/* If a user is logged in, show these links */}
-//         {user.id && (
-//           <>
-
-//             <Link className="navLink" to="/echarts">
-//               echarts
-//             </Link>
-
-//             <Link className="navLink" to="/familySize">
-//               family size
-//             </Link>
-
-
-
-//             <Link className="navLink" to="/user">
-//               Home
-//             </Link>
-
-//             <Link className="navLink" to="/info">
-//               Info Page
-//             </Link>
-
-//             <Link className="navLink" to="/my_trips">
-//               My Trips
-//             </Link>
-
-//             <LogOutButton className="navLink" />
-//           </>
-//         )}
-
-//         <Link className="navLink" to="/about">
-//           About
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Nav;
 
