d8b43f3b0dd0aeb88f9748f2d4cf4a653f54fac1
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2024-02-26 18:00:25 -0500

    styling contact

--- src/components/Contact/ContactG.jsx
+++ src/components/Contact/ContactG.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Button, TextField } from "@mui/material";
+import "./ContactG.css";
 
 function ContactG() {
   const dispatch = useDispatch();
@@ -33,146 +34,149 @@ function ContactG() {
 
   return (
     <>
-      <Button
-        style={{
-          position: 'absolute',
-          top: 70,
-          left: '10%',
-          transform: 'translateX(-50%)',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          textDecoration: 'none',
-          color: 'black',
-          fontWeight: 'bold',
-          fontSize: 'large',
-          textShadow: '9px 2px 9px white',
-        }}
-      >
-      </Button>
+      <h1 style={{
+        color: '',
+        textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        fontFamily: 'Dancing Script, cursive',
+        textAlign: 'center',
+      }}>Ready to take the first step towards a life of purpose, fulfillment, and abundance? Let's embark on this transformative journey together!</h1>
 
-      <form className='guest-form' onSubmit={handleSubmit}>
-        <h1 style={{
-          color: '',
-          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
-          fontFamily: 'Dancing Script, cursive',
-        }}>Ready to take the first step towards a life of purpose, fulfillment, and abundance? Let's embark on this transformative journey together.Lets Get Started!</h1>
 
-        <TextField
-          name="name"
-          label="Name"
-          value={formValues.name}
-          onChange={handleChange}
-          required
-          InputProps={{
-            style: {
-              color: '#6792DB',
-              fontFamily: "Georgia",
-              fontWeight: 'bolder',
-              fontSize: '25px',
-              textShadow: '11px 1px 2px rgba(0, 0, 0, 0.8)'
-            },
-          }}
-          InputLabelProps={{
-            style: {
-              color: '#E6B397',
-              fontFamily: "Georgia",
-              fontWeight: 'bolder',
-              fontSize: '25px',
-              textShadow: '1px 1px 2px rgba(0, 0, 0, 2.9)'
-            },
-          }}
-        />
-        <br />
-        <TextField
-          name="phone"
-          label="Phone"
-          value={formValues.phone}
-          onChange={handleChange}
-          required
-          InputProps={{
-            style: {
-              color: '#6792DB',
-              fontFamily: "Georgia",
-              fontWeight: 'bolder',
-              fontSize: '25px',
-              textShadow: '11px 1px 2px rgba(0, 0, 0, 0.8)'
-            },
-          }}
-          InputLabelProps={{
-            style: {
-              color: '#E6B397',
-              fontFamily: "Georgia",
-              fontWeight: 'bolder',
-              fontSize: '25px',
-              textShadow: '1px 1px 2px rgba(0, 0, 0, 2.9)'
-            },
-          }}
-        />
-        <br />
-        <TextField
-          name="email"
-          label="Email"
-          value={formValues.email}
-          onChange={handleChange}
-          required
-          InputProps={{
-            style: {
-              color: '#6792DB',
-              fontFamily: "Georgia",
-              fontWeight: 'bolder',
-              fontSize: '25px',
-              textShadow: '11px 1px 2px rgba(0, 0, 0, 0.8)'
-            },
-          }}
-          InputLabelProps={{
-            style: {
-              color: '#E6B397',
-              fontFamily: "Georgia",
-              fontWeight: 'bolder',
-              fontSize: '25px',
-              textShadow: '1px 1px 2px rgba(0, 0, 0, 2.9)'
-            },
-          }}
-        />
-        <br />
-        <TextField
-          name="message"
-          label="Message"
-          value={formValues.message}
-          onChange={handleChange}
-          required
-          InputProps={{
-            style: {
-              color: '#6792DB',
-              fontFamily: "Georgia",
-              fontWeight: 'bolder',
-              fontSize: '25px',
-              textShadow: '11px 1px 2px rgba(0, 0, 0, 0.8)'
-            },
-          }}
-          InputLabelProps={{
-            style: {
-              color: '#E6B397',
-              fontFamily: "Georgia",
-              fontWeight: 'bolder',
-              fontSize: '25px',
-              textShadow: '1px 1px 2px rgba(0, 0, 0, 2.9)'
-            },
-          }}
-        />
-        <br />
-        
-        <Button type="submit" variant="contained"
-          style={{
-            backgroundColor: '#E6B397',
-            color: 'white',
-            textShadow: '10px 10px 15px rgba(0.5, 0.5, 0.5, 3)',
-            boxShadow: '4px 4px 6px rgba(0, 0, 0, 0.5)',
-            fontFamily: 'Georgia',
-          }}>
-          Submit
-        </Button>
+<form onSubmit={handleSubmit} style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+         maxWidth: '1100px',
+         padding: '5px',
+       
+         
+      }}>
+        <div>
+          <div>
+            <TextField
+              name="name"
+              label="Name"
+              value={formValues.name}
+              onChange={handleChange}
+              required
+              inputProps={{
+                style: {
+                  color: '#6792DB',
+                  fontFamily: "lobster",
+                  fontWeight: 'bold',
+                  fontSize: '25px',
+                  textShadow:'2px 2px 4px rgba(0, 0, 0, 0.5)' ,
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: '#E6B397',
+                  fontFamily: "Georgia",
+                  fontWeight: 'bolder',
+                  fontSize: '25px',
+                  textShadow: '1px 1px 2px rgba(0, 0, 0, 2.9)'
+                },
+              }}
+            />
+          </div>
+          <div>
+            <TextField
+              name="phone"
+              label="Phone"
+              value={formValues.phone}
+              onChange={handleChange}
+              required
+              inputProps={{
+                style: {
+                  color: '#6792DB',
+                  fontFamily: "Georgia",
+                  fontWeight: 'bolder',
+                  fontSize: '25px',
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: '#E6B397',
+                  fontFamily: "Georgia",
+                  fontWeight: 'bolder',
+                  fontSize: '25px',
+                  textShadow: '1px 1px 2px rgba(0, 0, 0, 2.9)'
+                },
+              }}
+            />
+          </div>
+          <div>
+            <TextField
+              name="email"
+              label="Email"
+              value={formValues.email}
+              onChange={handleChange}
+              required
+              inputProps={{
+                style: {
+                  color: '#6792DB',
+                  fontFamily: "Georgia",
+                  fontWeight: 'bolder',
+                  fontSize: '25px',
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: '#E6B397',
+                  fontFamily: "Georgia",
+                  fontWeight: 'bolder',
+                  fontSize: '25px',
+                  textShadow: '1px 1px 2px rgba(0, 0, 0, 2.9)'
+                },
+              }}
+            />
+          </div>
+          <div>
+            <TextField
+              name="message"
+              label="Message"
+              value={formValues.message}
+              onChange={handleChange}
+              required
+              multiline
+              rows={4}
+              inputProps={{
+                style: {
+                  color: '#6792DB',
+                  fontFamily: "Georgia",
+                  fontWeight: 'bolder',
+                  fontSize: '25px',
+                  textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'
+                },
+              }}
+              InputLabelProps={{
+                style: {
+                  color: '#E6B397',
+                  fontFamily: "Georgia",
+                  fontWeight: 'bolder',
+                  fontSize: '25px',
+                  textShadow: '1px 1px 2px rgba(0, 0, 0, 2.9)'
+                },
+              }}
+            />
+          </div>
+
+          <Button type="submit" variant="contained"
+            style={{
+              backgroundColor: '#E6B397',
+              color: 'white',
+              textShadow: '10px 10px 15px rgba(0.5, 0.5, 0.5, 3)',
+              boxShadow: '4px 4px 6px rgba(0, 0, 0, 0.5)',
+              fontFamily: 'Georgia',
+            }}>
+            Submit
+          </Button>
+
+        </div >
       </form>
     </>
   )
