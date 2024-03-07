30277e4224be887bf6f7de951cb431b4a3120a9c
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2024-03-06 19:42:24 -0500

    contact working

--- server/server.js
+++ server/server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+
+
+
+
 
 require('dotenv').config();
 
@@ -30,6 +35,42 @@ app.use('/api/pets', petRouter)
 app.use('/api/patients', patientsRouter); 
 // Serve static files
 app.use(express.static('build'));
+
+
+//!email 
+app.post('/api/submit-form', async (req, res) => {
+  const { name, phone, email, message } = req.body;
+
+  // Create a transporter object using SMTP transport
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  // Email options
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: 'test.levietech123@gmail.com', //  recipient's email address
+    subject: 'New Form Submission',
+    text: `Name: ${name}\nPhone: ${phone}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  // Send email
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+    res.status(200).json({ message: 'Form submitted successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ message: 'Error sending email' });
+  }
+});
+
+
+
 
 // App Set //
 const PORT = process.env.PORT || 5999;
