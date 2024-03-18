f288d75b999a7d56dcab45bad54d3603a9b5b681
Julie Gonzalez-Kincaid <juliegonzalezkincaid@gmail.com>
2024-03-18 18:10:46 -0500

    sun icon

--- src/components/FloatingIcon/FloatingIcon.jsx
+++ src/components/FloatingIcon/FloatingIcon.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Avatar from '@mui/material/Avatar';
 import { Typography } from "@mui/material";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 function FloatingIcon() {
   return (
     <div style={{
+      display: 'flex',
+      alignItems: 'center',
       position: 'fixed',
       bottom: '20px',
       right: '20px',
@@ -15,16 +18,18 @@ function FloatingIcon() {
       zIndex: '1000', // Ensure it appears above other content
     }}>
       <a href="https://calendly.com/gninag/fit-call" target="_blank" rel="noopener noreferrer">
+      <WbSunnyIcon fontSize="medium" style={{ color: '#DBAD00', filter: "drop-shadow(3px 2px 4px rgba(0, 0, 0, 2.5))", position: 'absolute', top: '1px', left: '50%', transform: 'translateX(-50%)' }} />
         <Avatar alt="Giannina's Photo" src="/images/Giannina.png" style={{ width: '50px', height: '50px' }} />
       </a>
+    
       <Typography style={{
         color: 'black',
         textShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)',
         fontFamily: 'italic',
         textAlign: 'center',
         fontSize: '14px',
-        marginTop: '5px',
-      }}>Click here to <br></br> schedule a chat</Typography>
+       
+      }}>Click to  schedule a chat</Typography>
     </div>
   );
 }
